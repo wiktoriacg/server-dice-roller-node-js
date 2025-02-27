e391458e4418b94bdf2254f3e772e61ae680439d
wiktoriacg <wiktoria.cioch.gradzik@gmail.com>
2025-02-26 19:29:11 -0600

    Updated code

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const port = process.env.PORT || 3000;
 
 // Enable CORS
 app.use(cors());
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public'))); 
-
+app.use(express.static('public'));
 // Endpoint to roll a dice
 app.get('/api/roll/:sides', (req, res) => {
   const sides = parseInt(req.params.sides);
